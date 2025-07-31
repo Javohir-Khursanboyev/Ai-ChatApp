0f44048d3c3d8c32eff5a9234f2115e7334f4459
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-07-31 22:42:21 +0500

    Add Helpers

---
+++ src/Ai-ChatApp.Service/Helpers/AuthHelper.cs
@@ -0,0 +1,31 @@
+﻿using Ai_ChatApp.Domain.Entities.Identity;
+using Ai_ChatApp.Domain.Entities.Users;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Ai_ChatApp.Service.Helpers;
+
+public static class AuthHelper
+{
+    public static string GenerateToken(User user, Role role)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenKey = Encoding.UTF8.GetBytes(EnvironmentHelper.JWTKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                 new Claim("Id", user.Id.ToString()),
+                 new Claim("Email", user.Email),
+                 new Claim(ClaimTypes.Role, role.Name)
+            }),
+            Expires = DateTime.UtcNow.AddHours(Convert.ToInt32(EnvironmentHelper.TokenLifeTimeInHours)),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
