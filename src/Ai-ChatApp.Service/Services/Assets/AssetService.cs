35e21785bf278f1c3df27b53c691edd455384bec
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-07-31 23:12:36 +0500

    add AssetService

---
+++ src/Ai-ChatApp.Service/Services/Assets/AssetService.cs
@@ -0,0 +1,49 @@
+﻿using Ai_ChatApp.Data.UnitOfWorks;
+using Ai_ChatApp.Domain.Entities.Resources;
+using Ai_ChatApp.Service.Extensions;
+using Ai_ChatApp.Service.Helpers;
+using Ai_ChatApp.Service.Validators.Assets;
+using Ai_ChatApp.Shared.DTOs.Assets;
+using Ai_ChatApp.Shared.Exceptions;
+using AutoMapper;
+
+namespace Ai_ChatApp.Service.Services.Assets;
+
+public sealed class AssetService(IMapper mapper, IUnitOfWork unitOfWork, AssetCreateModelValidator createModelValidator) : IAssetService
+{
+    public async Task<AssetViewModel> UploadAsync(AssetCreateModel model)
+    {
+        await createModelValidator.EnsureValidatedAsync(model);
+
+        var assetData = await FileHelper.CreateFileAsync(model);
+        var asset = new Asset()
+        {
+            Name = assetData.Name,
+            Path = assetData.Path,
+            FileType = model.FileType
+        };
+
+        var createdAsset = await unitOfWork.Assets.InsertAsync(asset);
+        await unitOfWork.SaveAsync();
+
+        return mapper.Map<AssetViewModel>(asset);
+    }
+
+    public async Task<bool> DeleteAsync(long id)
+    {
+        var existAsset = await unitOfWork.Assets.SelectAsync(asset => asset.Id == id)
+            ?? throw new NotFoundException("Asset is not found");
+
+        await unitOfWork.Assets.DropAsync(existAsset);
+        return await unitOfWork.SaveAsync();
+    }
+
+    public async Task<AssetViewModel> GetByIdAsync(long id)
+    {
+        var existAsset = await unitOfWork.Assets.SelectAsync(asset => asset.Id == id)
+           ?? throw new NotFoundException("Asset is not found");
+
+        return mapper.Map<AssetViewModel>(existAsset);
+    }
+
+}
---
+++ src/Ai-ChatApp.Service/Services/Assets/IAssetService.cs
@@ -0,0 +1,10 @@
+﻿using Ai_ChatApp.Shared.DTOs.Assets;
+
+namespace Ai_ChatApp.Service.Services.Assets;
+
+public interface IAssetService
+{
+    Task<AssetViewModel> UploadAsync(AssetCreateModel model);
+    Task<bool> DeleteAsync(long id);
+    Task<AssetViewModel> GetByIdAsync(long id);
+}
